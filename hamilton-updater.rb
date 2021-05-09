eec2e909312f465784b9bf6a59240a243d8ff553
Ryan Punt <ryan@mirum.org>
2021-05-09 13:57:58 -0500

    find containing directory for script

--- hamilton-updater.rb
+++ hamilton-updater.rb
@@ -20,23 +20,23 @@ race_status = JSON.parse(response.body)
 raceinfo = race_status['seasonContext']['timetables'].select{|hash| hash['description'] == 'Race' }.first
 raceStart = Time.parse("#{raceinfo['startTime']} #{raceinfo['gmtOffset']}").utc
 
-config = YAML.load_file('_config.yml')
+config = YAML.load_file("#{__dir__}/_config.yml")
 config['description'] = "#{race_status['race']['meetingCountryName']}: #{raceStart.strftime("%d %B %Y")}"
-File.write('_config.yml', config.to_yaml)
+File.write("#{__dir__}/_config.yml", config.to_yaml)
 
 case raceinfo['state']
 when 'upcoming'
   puts "race hasn't started yet"
-  File.write('index.md', "# Race hasn't started yet")
+  File.write("#{__dir__}/index.md", "# Race hasn't started yet")
 when 'started'
-  File.write('index.md', '# Racing now')
+  File.write("#{__dir__}/index.md", '# Racing now')
 when 'completed'
   winner = race_status['raceResults'].select{|hash| hash['positionNumber'] == '1' }.first
   answer = winner['driverLastName'].downcase == 'hamilton' ? 'YES' : 'NO'
-  File.write('index.md', "# #{answer}.")
+  File.write("#{__dir__}/index.md", "# #{answer}.")
 end
 
-g = Git.open(ENV['PWD'])
+g = Git.open(__dir__)
 if g.status.changed.count > 0
   g.add(:all=>true)
   g.commit_all("updating for ${gp}: ${date}; #{raceinfo['state']}")
