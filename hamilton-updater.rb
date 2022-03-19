43e6a28b8402061eccecdc10f52abf114602e9be
Ryan Punt <ryan@mirum.org>
2022-03-19 10:16:26 -0500

    Revert "updating for Bahrain: 20 March 2022; race upcoming"
    This reverts commit 5f39f0dc51f8dde286d35e8a0a79799d8caa876a.

--- hamilton-updater.rb
+++ hamilton-updater.rb
@@ -23,18 +23,11 @@ rescue StandardError => e
 end
 
 race_status = JSON.parse(response.body)
-qualiInfo = race_status['seasonContext']['timetables'].select{|hash| hash['description'] == 'Qualifying' }.first
 raceinfo = race_status['seasonContext']['timetables'].select{|hash| hash['description'] == 'Race' }.first
-
-require 'pry'; binding.pry
-
-qualiStart = Time.parse("#{qualiInfo['startTime']} #{qualiInfo['gmtOffset']}").utc
 raceStart = Time.parse("#{raceinfo['startTime']} #{raceinfo['gmtOffset']}").utc
 
 config = YAML.load_file("#{__dir__}/_config.yml")
 config['description'] = "#{race_status['race']['meetingCountryName']}: #{raceStart.strftime("%d %B %Y")}"
-config['quali_time'] = qualiStart.strftime("%H:%M GMT")
-config['race_time'] = raceStart.strftime("%H:%M GMT")
 File.write("#{__dir__}/_config.yml", config.to_yaml)
 
 case raceinfo['state']
