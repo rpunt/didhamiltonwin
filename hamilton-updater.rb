422d7fd95e3a29f2f1cca20cbdf6a95aba4cbbd1
Ryan Punt <ryan@mirum.org>
2022-08-28 00:01:49 -0500

    rubocop: easy fixes

--- hamilton-updater.rb
+++ hamilton-updater.rb
@@ -13,7 +13,7 @@ headers = {
 }
 
 begin
-  response = HTTParty.get( 'https://api.formula1.com/v1/event-tracker', :headers => headers )
+  response = HTTParty.get('https://api.formula1.com/v1/event-tracker', headers: headers)
 rescue HTTParty::Error => e
   puts "had an error: #{e}"
 rescue StandardError => e
@@ -21,11 +21,11 @@ rescue StandardError => e
 end
 
 race_status = JSON.parse(response.body)
-raceinfo = race_status['seasonContext']['timetables'].select{|hash| hash['description'] == 'Race' }.first
+raceinfo = race_status['seasonContext']['timetables'].select { |hash| hash['description'] == 'Race' }.first
 raceStart = Time.parse("#{raceinfo['startTime']} #{raceinfo['gmtOffset']}").utc
 
 config = YAML.load_file("#{__dir__}/_config.yml")
-config['description'] = "#{race_status['race']['meetingCountryName']}: #{raceStart.strftime("%d %B %Y")}"
+config['description'] = "#{race_status['race']['meetingCountryName']}: #{raceStart.strftime('%d %B %Y')}"
 File.write("#{__dir__}/_config.yml", config.to_yaml)
 
 case raceinfo['state']
@@ -34,14 +34,14 @@ when 'upcoming'
 when 'started'
   answer = "# They're racing now"
 when 'completed'
-  winner = race_status['raceResults'].select{|hash| hash['positionNumber'] == '1' }.first
+  winner = race_status['raceResults'].select { |hash| hash['positionNumber'] == '1' }.first
   answer = winner['driverLastName'].downcase == 'hamilton' ? 'YES' : 'NO'
 end
 File.write("#{__dir__}/index.md", "# #{answer}")
 File.write("#{__dir__}/api_results/#{Time.now.strftime('%Y%m%d-%H%M%S')}.json", JSON.pretty_generate(race_status))
 
 if g.status.changed.count > 0
-  g.add(:all=>true)
-  g.commit_all("updating for #{race_status['race']['meetingCountryName']}: #{raceStart.strftime("%d %B %Y")}; race #{raceinfo['state']}")
+  g.add(all: true)
+  g.commit_all("updating for #{race_status['race']['meetingCountryName']}: #{raceStart.strftime('%d %B %Y')}; race #{raceinfo['state']}")
   g.push(g.remote('origin'))
 end
